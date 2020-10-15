8ce3c3ad924c4cdb6dba3d736e987206bf700a74
Ankit <ankitmitra486@gmail.com>
2020-10-15 17:07:57 +0530

    My First commit!

---
+++ django3-personal-portfolio-master/portfolio/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Project
+
+def home(request):
+    projects = Project.objects.all()
+    return render(request, 'portfolio/home.html', {'projects':projects})
+
+def about(request):
+    return render (request,'portfolio/about.html')
